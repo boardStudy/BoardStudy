522e65a85d5a82ccf950aad8684c48381d5ba34c
이호인 <lovi0714@kyonggi.ac.kr>
2022-03-21 19:31:28 +0900

    Refactoring : @어노테이션 제거
    Configure 클래스에서 따로 Bean 등록을 해주었기 때문에 제거

--- src/main/java/com/hoin/boardStudy/board/config/handler/PageRequestHandler.java
+++ src/main/java/com/hoin/boardStudy/board/config/handler/PageRequestHandler.java
@@ -2,13 +2,11 @@ package com.hoin.boardStudy.board.config.handler;
 
 import com.hoin.boardStudy.board.dto.PageInfo;
 import org.springframework.core.MethodParameter;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
-@Component
 public class PageRequestHandler implements HandlerMethodArgumentResolver {
     @Override
     public boolean supportsParameter(MethodParameter parameter) {

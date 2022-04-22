ad12518339513ef22db2379385f7378ad2c6050a
dahoon <dahoon1006@gmail.com>
2022-04-22 23:49:54 +0900

    Feature : 회원가입 이메일 인증
    1. 이메일 전송 기능(완료)
    2. 전송 받은 인증번호 처리(진행중)

--- src/main/java/com/hoin/boardStudy/user/controller/UserController.java
+++ src/main/java/com/hoin/boardStudy/user/controller/UserController.java
@@ -4,9 +4,14 @@ import com.hoin.boardStudy.user.dto.User;
 import com.hoin.boardStudy.user.service.LoginVerification;
 import com.hoin.boardStudy.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import javax.mail.internet.MimeMessage;
 import javax.servlet.http.HttpSession;
 
 @Controller
@@ -16,6 +21,8 @@ public class UserController {
 
     private final UserService userService;
     private final LoginVerification loginVerification;
+
+    private final JavaMailSender javaMailSender;
 
     /* 로그인 화면 */
     @GetMapping("/login.do")
@@ -93,6 +100,36 @@ public class UserController {
         session.invalidate();
 
         return "redirect:/board/list.do";
+    }
+
+    @ResponseBody
+    @GetMapping("/mailCheck")
+
+    public String mailCheck(@RequestParam("email") String email) throws Exception {
+        int serti = (int)((Math.random() * (99999 - 10000 + 1)) + 10000);
+
+        String from = "dahoonemailtest@gmail.com"; //보내는 이 메일주소
+        String to = email;
+        String title = "회원가입시 필요한 인증번호 입니다.";
+        String content = "[인증번호] " + serti + " 입니다. <br/> 인증번호 확인란에 기입해주십시오.";
+        String num = "";
+
+        try {
+            MimeMessage mail = javaMailSender.createMimeMessage();
+            MimeMessageHelper mailHelper = new MimeMessageHelper(mail, true, "UTF-8");
+
+            mailHelper.setFrom(from);
+            mailHelper.setTo(to);
+            mailHelper.setSubject(title);
+            mailHelper.setText(content, true);
+
+            javaMailSender.send(mail);
+            num = Integer.toString(serti);
+
+        } catch (Exception e) {
+            num = "error";
+        }
+        return num; // 인증번호
     }
 
 

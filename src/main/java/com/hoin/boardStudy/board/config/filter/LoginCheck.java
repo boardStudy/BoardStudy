4abf3b16f454e2093f551acc6b8ea8c8ad063c9a
이호인 <lovi0714@kyonggi.ac.kr>
2022-05-08 01:47:27 +0900

    안 쓰는 import문 정리

--- src/main/java/com/hoin/boardStudy/board/config/filter/LoginCheck.java
+++ src/main/java/com/hoin/boardStudy/board/config/filter/LoginCheck.java
@@ -1,7 +1,6 @@
 package com.hoin.boardStudy.board.config.filter;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.util.PatternMatchUtils;
 
@@ -11,7 +10,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.util.Arrays;
 
 @Component
 @Slf4j

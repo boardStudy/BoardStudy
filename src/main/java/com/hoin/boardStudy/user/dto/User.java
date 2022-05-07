cd716e54ef4c9ebbf1518b1f82caff31585e2150
dahoon <dahoon1006@gmail.com>
2022-05-08 03:17:42 +0900

    wip

--- src/main/java/com/hoin/boardStudy/user/dto/User.java
+++ src/main/java/com/hoin/boardStudy/user/dto/User.java
@@ -32,9 +32,10 @@ public class User implements Serializable {
     @NotBlank(message = "이름을 입력해주세요.")
     private String name;
 
-    @NotBlank(message = "전화번호를 입력해주세요.")
+    @NotBlank(message = "휴대폰 번호를 입력해주세요.")
     @Pattern(regexp = "(01[016789])(\\d{3,4})(\\d{4})", message = "올바른 휴대폰 번호를 입력해주세요.")
     private String phone;
+
     private int gender;
 
     @Builder

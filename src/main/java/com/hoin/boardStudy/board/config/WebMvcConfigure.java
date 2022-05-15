a7f4116266fca1192eff6f7fc11f097920f8a5b6
이호인 <lovi0714@kyonggi.ac.kr>
2022-05-15 13:53:53 +0900

    Json에 대한 XSS 방어

--- src/main/java/com/hoin/boardStudy/board/config/WebMvcConfigure.java
+++ src/main/java/com/hoin/boardStudy/board/config/WebMvcConfigure.java
@@ -1,11 +1,15 @@
 package com.hoin.boardStudy.board.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hoin.boardStudy.board.config.filter.LoginCheck;
+import com.hoin.boardStudy.board.config.filter.HtmlCharacterEscapes;
 import com.hoin.boardStudy.board.config.handler.PageRequestHandler;
 import com.navercorp.lucy.security.xss.servletfilter.XssEscapeServletFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,16 +17,22 @@ import javax.servlet.Filter;
 import java.util.List;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebMvcConfigure implements WebMvcConfigurer {
+
+    private final ObjectMapper objectMapper;
+
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(PageRequestHandler());
     }
 
+    // 페이징 파라미터
     public PageRequestHandler PageRequestHandler() {
         return new PageRequestHandler();
     }
-
+    
+    // 로그인 여부 확인
     public FilterRegistrationBean loginCheck() {
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new LoginCheck());
@@ -40,5 +50,13 @@ public class WebMvcConfigure implements WebMvcConfigurer {
         filterRegistrationBean.setOrder(2);
         filterRegistrationBean.addUrlPatterns("/*");
         return filterRegistrationBean;
+    }
+    
+    // json xss 방어
+    @Bean
+    public MappingJackson2HttpMessageConverter jsonEscapeConverter() {
+        ObjectMapper copy = objectMapper.copy();
+        copy.getFactory().setCharacterEscapes(new HtmlCharacterEscapes());
+        return new MappingJackson2HttpMessageConverter(copy);
     }
 }
